d60e6acdee0a2b6899140ba4e1348323c74d324e
tengfei1009 <145204810+tengfei1009@users.noreply.github.com>
2024-07-25 11:53:30 +0800

    测试

--- src/views/demo/index.vue
+++ src/views/demo/index.vue
@@ -5,7 +5,8 @@ import NavBar from "@/components/NavBar/index.vue";
 </script>
 
 <template> 
-<p>我的大学生活</p>
+
+<p>我的大学生活121</p>
 <p>与你相遇的第{{}}天</p>
 <i-icon icon="icon-park:read-book"></i-icon>
  首页demo
