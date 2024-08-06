f3ad67d9e7ddce4c10e06bd1b232ef6db0ee54c5
hanshuobo <1432101420@qq.com>
2024-08-06 21:00:16 +0800

    feat: code

--- src/views/demo/index.vue
+++ src/views/demo/index.vue
@@ -96,17 +96,47 @@ onMounted(async () => {
       <i-icon icon="icon-park:read-book" class="text-xl" />
     </span>
   </div>
-  <van-search v-model="inputValue" show-action shape="round" placeholder="请输入搜索关键词" background="#fff" class="search"
-    @search="onSearch(1)">
+  <van-search
+    v-model="inputValue"
+    show-action
+    shape="round"
+    placeholder="请输入搜索关键词"
+    background="#fff"
+    class="search"
+    @search="onSearch(1)"
+  >
     <template #action>
-      <van-button type="primary" size="small" color="#004ff7" round @click="onClickButton">搜索</van-button>
+      <van-button
+        type="primary"
+        size="small"
+        color="#004ff7"
+        round
+        @click="onClickButton"
+        >搜索</van-button
+      >
     </template>
   </van-search>
 
-  <van-tabs v-model:active="activeName" background="#f0f1f5" color="#041833" swipeable sticky>
-    <van-tab v-for="item in topicList" :key="item.title" :title="item.title" :name="item.name">
+  <van-tabs
+    v-model:active="activeName"
+    background="#f0f1f5"
+    color="#041833"
+    swipeable
+    sticky
+  >
+    <van-tab
+      v-for="item in topicList"
+      :key="item.title"
+      :title="item.title"
+      :name="item.name"
+    >
       <van-pull-refresh v-model="refreshing" @refresh="onRefresh">
-        <van-list v-model:loading="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+        <van-list
+          v-model:loading="loading"
+          :finished="finished"
+          finished-text="没有更多了"
+          @load="onLoad"
+        >
           <cell-card v-for="item in list" :key="item" @click="console.log(1)" />
         </van-list>
       </van-pull-refresh>
@@ -139,12 +169,12 @@ span {
 
 /* 样式穿透,强制找到子组件 */
 
-.van-search>>>.van-search__content {
+.van-search >>> .van-search__content {
   background-color: #fff !important;
   background: #fff;
 }
 
-.van-search>>>.van-search__action {
+.van-search >>> .van-search__action {
   padding: 0px;
   display: flex;
 }
