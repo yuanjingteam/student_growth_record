9f4267cca782ee01144f812ea1b9fcf1f58b92ed
hanshuobo <1432101420@qq.com>
2024-08-07 14:29:55 +0800

    feat: merge han and develop

--- src/views/demo/index.vue
+++ src/views/demo/index.vue
@@ -36,6 +36,8 @@ const searchData = reactive({
   article_count: "5"
 });
 
+console.log("aaaa");
+
 //获取注册天数
 const registerDay = async () => {
   const { data } = await getRegisterDay({ username: username });
