c59538e343d42c50915c4ccedf7acf3a9be4546e
tengfei1009 <3240288774@qq.com>
2024-08-25 11:31:31 +0800

    feat: system_headshot_gu_din

--- src/components/systemItem/index.vue
+++ src/components/systemItem/index.vue
@@ -5,7 +5,7 @@ const props = defineProps({
   data: Object
 });
 let userImg =
-  "https://student-grow.oss-cn-beijing.aliyuncs.com/image/system.png";
+  "https://student-grow.oss-cn-beijing.aliyuncs.com/image/system_2.png";
 const formattedContent = content => {
   // 使用正则表达式替换 <br/> 标签为换行符
   return content.replace(/<br\s*\/?>/g, "\n");
