d0687c4a4cb25724ebc218f0f9aeb73231cdffdd
tengfei1009 <3240288774@qq.com>
2024-10-11 20:48:21 +0800

    feat:improve_self_info

--- src/components/manageItem/index.vue
+++ src/components/manageItem/index.vue
@@ -1,9 +1,32 @@
-<script setup>
+<script setup lang="ts">
+import { ref } from "vue";
+import { useUserStore } from "@/store";
 import { formattedContent } from "@/utils/functions/modules/formattedContent";
+import { delManagerInfo } from "@/api/user";
+import { showToast } from "vant";
 
+const userStore = useUserStore();
 const props = defineProps({
   data: Object
 });
+const emit = defineEmits(["refresh"]);
+const showPopover = ref(false);
+const actions = [{ text: "删除" }];
+
+// 选择选项
+const onSelect = async () => {
+  delInfo(props.data.info_id);
+};
+
+const delInfo = async (id: string) => {
+  try {
+    await delManagerInfo({ id });
+    emit("refresh");
+    showToast("删除成功");
+  } catch (error) {
+    showToast(error.msg);
+  }
+};
 // let userImg = "https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg";
 </script>
 <template>
@@ -19,8 +42,21 @@ const props = defineProps({
         <van-text-ellipsis :content="data.msg_time" />
       </template>
       <template #right-icon>
+        <!-- userStore.role === 'manager' -->
         <div v-if="true">
-          <!-- <i-icon icon="ri:more-fill" /> -->
+          <van-popover
+            v-model:show="showPopover"
+            :actions="actions"
+            @select="onSelect"
+          >
+            <template #reference>
+              <div class="more">
+                <span>
+                  <i-icon icon="ri:more-fill" />
+                </span>
+              </div>
+            </template>
+          </van-popover>
         </div>
       </template>
     </van-cell>
@@ -46,5 +82,8 @@ const props = defineProps({
 }
 .van-image {
   margin: 2px 8px 0px 0px;
+}
+.van-popup {
+  padding-right: 60px;
 }
 </style>
